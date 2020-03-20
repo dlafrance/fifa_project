97d4d20ab60e35e6a25c979637fc3bcb92e640df
dlafrance <dlafrance21@gmail.com>
2020-03-19 21:00:19 -0400

    Next go

--- logistic.py
+++ logistic.py
@@ -36,10 +36,10 @@ def simple_position(df):
         return 'DEF'
     elif ((df['team_position'] == 'LM') | (df['team_position'] == 'LCM') | (df['team_position'] == 'CM') |
           (df['team_position'] == 'RCM') | (df['team_position'] == 'RM')):
-        return 'MF'
+        return 'MID'
     elif ((df['team_position'] == 'LAM') | (df['team_position'] == 'CAM') | (df['team_position'] == 'RAM') |
           (df['team_position'] == 'LW') | (df['team_position'] == 'RW')):
-        return 'MF'
+        return 'MID'
     elif ((df['team_position'] == 'RS') | (df['team_position'] == 'ST') | (df['team_position'] == 'LS') |
           (df['team_position'] == 'CF') | (df['team_position'] == 'LF') | (df['team_position'] == 'RF')):
         return 'FWD'
@@ -57,52 +57,95 @@ df = df.dropna()
 print(df.isnull().sum())
 print(df.shape)
 
-# linear regression
-X = df.drop(['position'], axis=1)
-y = df['position']
+# Distribution of rating overall
+sns.set(style="dark", palette="deep", color_codes=True)
+sns.distplot(df['overall'], kde=False, bins=20, color='b')
+plt.title('Distribution of FIFA Rating')
+plt.xlabel('Bins')
+plt.ylabel('Frequency')
+plt.savefig('plots/hist_rating.png')
+plt.clf()
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# # Distribution of rating by position
+# f, axes = plt.subplots(2, 2, figsize=(8, 8), sharex=True)
+# sns.distplot(df['overall'][df['position'] == 'FWD'], color="skyblue", ax=axes[0, 0])
+# sns.distplot(df['overall'][df['position'] == 'MID'], color="olive", ax=axes[0, 1])
+# sns.distplot(df['overall'][df['position'] == 'DEF'], color="gold", ax=axes[1, 0])
+# sns.distplot(df['overall'][df['position'] == 'GK'], color="teal", ax=axes[1, 1])
+# plt.savefig('plots/hist_rating_position.png')
+# plt.clf()
 
-scaler = StandardScaler()
-scaler.fit(X)
-scaled_features = scaler.transform(X)
+# Charting overall rating by position
+f, ax = plt.subplots(figsize=(6, 6))
+sns.stripplot(x='position', y='overall', data=df, jitter=0.2, palette="Set2", dodge=True)
+plt.title('Position and FIFA Rating')
+plt.xlabel('Position')
+plt.ylabel('FIFA Rating')
+plt.savefig('plots/scatter_position_rating.png')
+plt.clf()
 
-# Splitting features and target datasets into: train and test
-X_train, X_test, y_train, y_test = train_test_split(scaled_features, y, test_size=0.3)
+# Box plot
+sns.set(style="dark", palette="deep", color_codes=True)
+position_order = ['FWD', 'MID', 'DEF', 'GK']
+sns.boxplot(x='position', y='overall', order=position_order, data=df)
+plt.title('Position and FIFA Rating')
+plt.xlabel('Position')
+plt.ylabel('FIFA Rating')
+plt.savefig('plots/box_position_rating.png')
+plt.clf()
 
-# Printing original Dataset
-print(f"X.shape: {X.shape}, y.shape: {y.shape}")
+# Correlation matrix
+sns.heatmap(df)
+plt.savefig('plots/corr_heatmap.png')
+plt.clf()
 
-# Printing splitted datasets
-print(f"X_train.shape: {X_train.shape}, y_train.shape: {y_train.shape}")
-print(f"X_test.shape: {X_test.shape}, y_test.shape: {y_test.shape}")
 
-# Training a Linear Regression model with fit()
-from sklearn.linear_model import LogisticRegression
-
-lr = LogisticRegression(max_iter=10000)
-lr.fit(X_train, y_train)
-
-# Predicting the results for our test dataset
-predicted_values = lr.predict(X_test)
-
-# Printing the residuals: difference between real and predicted
-for (real, predicted) in list(zip(y_test, predicted_values)):
-    print(f'Value: {real}, pred: {predicted} {"is different" if real != predicted else ""}')
-
-# Printing accuracy score(mean accuracy) from 0 - 1
-print(f'Accuracy score is {lr.score(X_test, y_test):.2f}/1 \n')
-
-# Printing the classification report
-from sklearn.metrics import classification_report, confusion_matrix, f1_score
-
-print('Classification Report')
-print(classification_report(y_test, predicted_values))
-
-# Printing the classification confusion matrix (diagonal is true)
-print('Confusion Matrix')
-print(confusion_matrix(y_test, predicted_values))
-
-print('Overall f1-score')
-print(f1_score(y_test, predicted_values, average="macro"))
+# # linear regression
+# X = df.drop(['position'], axis=1)
+# y = df['position']
+#
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+#
+# scaler = StandardScaler()
+# scaler.fit(X)
+# scaled_features = scaler.transform(X)
+#
+# # Splitting features and target datasets into: train and test
+# X_train, X_test, y_train, y_test = train_test_split(scaled_features, y, test_size=0.3)
+#
+# # Printing original Dataset
+# print(f"X.shape: {X.shape}, y.shape: {y.shape}")
+#
+# # Printing splitted datasets
+# print(f"X_train.shape: {X_train.shape}, y_train.shape: {y_train.shape}")
+# print(f"X_test.shape: {X_test.shape}, y_test.shape: {y_test.shape}")
+#
+# # Training a Linear Regression model with fit()
+# from sklearn.linear_model import LogisticRegression
+#
+# lr = LogisticRegression(max_iter=10000)
+# lr.fit(X_train, y_train)
+#
+# # Predicting the results for our test dataset
+# predicted_values = lr.predict(X_test)
+#
+# # Printing the residuals: difference between real and predicted
+# for (real, predicted) in list(zip(y_test, predicted_values)):
+#     print(f'Value: {real}, pred: {predicted} {"is different" if real != predicted else ""}')
+#
+# # Printing accuracy score(mean accuracy) from 0 - 1
+# print(f'Accuracy score is {lr.score(X_test, y_test):.2f}/1 \n')
+#
+# # Printing the classification report
+# from sklearn.metrics import classification_report, confusion_matrix, f1_score
+#
+# print('Classification Report')
+# print(classification_report(y_test, predicted_values))
+#
+# # Printing the classification confusion matrix (diagonal is true)
+# print('Confusion Matrix')
+# print(confusion_matrix(y_test, predicted_values))
+#
+# print('Overall f1-score')
+# print(f1_score(y_test, predicted_values, average="macro"))
