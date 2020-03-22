205b5d3914a736b9168967ac85b3a2991617fa2b
dlafrance <dlafrance21@gmail.com>
2020-03-22 18:23:41 -0400

    Position charts

--- logistic.py
+++ logistic.py
@@ -15,9 +15,9 @@ print(df.team_position.value_counts())
 print(df.shape)
 print(df.columns.values.tolist())
 
-df = df[['overall', 'player_positions', 'team_position', 'attacking_crossing', 'attacking_finishing',
-         'attacking_heading_accuracy',
-         'attacking_short_passing', 'attacking_volleys', 'skill_dribbling', 'skill_curve', 'skill_fk_accuracy',
+df = df[['overall', 'age', 'height_cm', 'weight_kg', 'player_positions', 'team_position', 'attacking_crossing',
+         'attacking_finishing', 'attacking_heading_accuracy', 'attacking_short_passing', 'attacking_volleys',
+         'skill_dribbling', 'skill_curve', 'skill_fk_accuracy',
          'skill_long_passing', 'skill_ball_control', 'movement_acceleration', 'movement_sprint_speed',
          'movement_agility', 'movement_reactions', 'movement_balance', 'power_shot_power', 'power_jumping',
          'power_stamina', 'power_strength', 'power_long_shots', 'mentality_aggression', 'mentality_interceptions',
@@ -39,14 +39,15 @@ df['team_position'].fillna(df['player_positions'], inplace=True)
 print(df.isnull().sum())
 print(df.team_position.value_counts())
 
+
 # Reduce number of positions
 def simple_position(df):
     if ((df['team_position'] == 'RB') | (df['team_position'] == 'LB') | (df['team_position'] == 'CB') |
             (df['team_position'] == 'LCB') | (df['team_position'] == 'RCB') | (df['team_position'] == 'RWB') |
             (df['team_position'] == 'LWB')):
         return 'DEF'
-    elif ((df['team_position'] == 'LDM') | (df['team_position'] == 'CDM') | (df['team_position'] == 'RDM')):
-        return 'DEF'
+    elif (df['team_position'] == 'LDM') | (df['team_position'] == 'CDM') | (df['team_position'] == 'RDM'):
+        return 'MID'
     elif ((df['team_position'] == 'LM') | (df['team_position'] == 'LCM') | (df['team_position'] == 'CM') |
           (df['team_position'] == 'RCM') | (df['team_position'] == 'RM')):
         return 'MID'
@@ -62,52 +63,91 @@ def simple_position(df):
 
 df['position'] = df.apply(simple_position, axis=1)
 
-# # Drop both original fields and remove NAs
+# # Drop both original fields
 df = df.drop(['player_positions', 'team_position'], axis=1)
 print(df.position.value_counts())
 print(df.shape)
 
-# print(df.isnull().sum())
-# df = df.dropna()
-# print(df.isnull().sum())
-# print(df.shape)
-# print(df.team_position.value_counts())
 
-# Distribution of rating overall
-# sns.set(style="dark", palette="deep", color_codes=True)
-# sns.distplot(df['overall'], kde=False, bins=20, color='b')
-# plt.title('Distribution of FIFA Rating')
-# plt.xlabel('Bins')
-# plt.ylabel('Frequency')
-# plt.savefig('plots/hist_rating.png')
-# plt.clf()
+# Box plot
+sns.set(style="dark", palette="GnBu_d", color_codes=True)
+position_order = ['FWD', 'MID', 'DEF', 'GK']
+sns.boxplot(x='position', y='overall', order=position_order, data=df)
+plt.title('Position and FIFA Rating')
+plt.xlabel('Position')
+plt.ylabel('FIFA Rating')
+plt.savefig('plots/box_position_rating.png')
+plt.clf()
 
-# # Distribution of rating by position
-# f, axes = plt.subplots(2, 2, figsize=(8, 8), sharex=True)
-# sns.distplot(df['overall'][df['position'] == 'FWD'], color="skyblue", ax=axes[0, 0])
-# sns.distplot(df['overall'][df['position'] == 'MID'], color="olive", ax=axes[0, 1])
-# sns.distplot(df['overall'][df['position'] == 'DEF'], color="gold", ax=axes[1, 0])
-# sns.distplot(df['overall'][df['position'] == 'GK'], color="teal", ax=axes[1, 1])
-# plt.savefig('plots/hist_rating_position.png')
-# plt.clf()
+# Distribution of skill rating by position
+f, axes = plt.subplots(2, 2, figsize=(8, 8), sharex=True)
+f.suptitle('Distribution of dribbling skill by position', fontsize=16)
+sns.set(style="dark", palette="GnBu_d", color_codes=True)
+sns.distplot(df['skill_dribbling'][df['position'] == 'FWD'], ax=axes[0, 0])
+axes[0][0].set(xlabel='FWD', ylabel='Frequency share')
+sns.distplot(df['skill_dribbling'][df['position'] == 'MID'], ax=axes[0, 1])
+axes[0][1].set(xlabel='MID')
+sns.distplot(df['skill_dribbling'][df['position'] == 'DEF'], ax=axes[1, 0])
+axes[1][0].set(xlabel='DEF', ylabel='Frequency share')
+sns.distplot(df['skill_dribbling'][df['position'] == 'GK'], ax=axes[1, 1])
+axes[1][1].set(xlabel='GK')
+plt.savefig('plots/hist_rating_position.png')
+plt.clf()
+
+player_features = ['attacking_crossing',
+                   'attacking_finishing', 'attacking_heading_accuracy', 'attacking_short_passing', 'attacking_volleys',
+                   'skill_dribbling', 'skill_curve', 'skill_fk_accuracy',
+                   'skill_long_passing', 'skill_ball_control', 'movement_acceleration', 'movement_sprint_speed',
+                   'movement_agility', 'movement_reactions', 'movement_balance', 'power_shot_power', 'power_jumping',
+                   'power_stamina', 'power_strength', 'power_long_shots', 'mentality_aggression',
+                   'mentality_interceptions',
+                   'mentality_positioning', 'mentality_vision', 'mentality_penalties', 'mentality_composure',
+                   'defending_marking', 'defending_standing_tackle', 'defending_sliding_tackle', 'goalkeeping_diving',
+                   'goalkeeping_handling', 'goalkeeping_kicking', 'goalkeeping_positioning', 'goalkeeping_reflexes']
+
+for i, val in df.groupby(df['position'])[player_features].mean().iterrows():
+    print('Position {}: {}, {}, {}'.format(i, *tuple(val.nlargest(4).index)))
+
+# from math import pi
 #
-# # Charting overall rating by position
-# f, ax = plt.subplots(figsize=(6, 6))
-# sns.stripplot(x='position', y='overall', data=df, jitter=0.2, palette="Set2", dodge=True)
-# plt.title('Position and FIFA Rating')
-# plt.xlabel('Position')
-# plt.ylabel('FIFA Rating')
-# plt.savefig('plots/scatter_position_rating.png')
-# plt.clf()
+# idx = 1
+# plt.figure(figsize=(15, 45))
+# for position_name, features in df.groupby(df['position'])[player_features].mean().iterrows():
+#     top_features = dict(features.nlargest(5))
 #
-# # Box plot
-# sns.set(style="dark", palette="deep", color_codes=True)
-# position_order = ['FWD', 'MID', 'DEF', 'GK']
-# sns.boxplot(x='position', y='overall', order=position_order, data=df)
-# plt.title('Position and FIFA Rating')
-# plt.xlabel('Position')
-# plt.ylabel('FIFA Rating')
-# plt.savefig('plots/box_position_rating.png')
+#     # number of variable
+#     categories = top_features.keys()
+#     N = len(categories)
+#
+#     # We are going to plot the first line of the data frame.
+#     # But we need to repeat the first value to close the circular graph:
+#     values = list(top_features.values())
+#     values += values[:1]
+#
+#     # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+#     angles = [n / float(N) * 2 * pi for n in range(N)]
+#     angles += angles[:1]
+#
+# # Initialise the spider plot
+# ax = plt.subplot(9, 3, idx, polar=True)
+#
+# # Draw one axe per variable + add labels labels yet
+# plt.xticks(angles[:-1], categories, color='grey', size=8)
+#
+# # Draw ylabels
+# ax.set_rlabel_position(0)
+# plt.yticks([25, 50, 75], ["25", "50", "75"], color="grey", size=7)
+# plt.ylim(0, 100)
+# plt.subplots_adjust(hspace=0.5)
+#
+# # Plot data
+# ax.plot(angles, values, linewidth=1, linestyle='solid')
+#
+# # Fill area
+# ax.fill(angles, values, 'b', alpha=0.1)
+# plt.title(position_name, size=11, y=1.1)
+# idx += 1
+# plt.savefig('plots/best_skills.png')
 # plt.clf()
 
 
@@ -117,9 +157,8 @@ y = df['position']
 
 from sklearn.model_selection import train_test_split
 
-
 # Splitting features and target datasets into: train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Printing original Dataset
 print(f"X.shape: {X.shape}, y.shape: {y.shape}")
@@ -156,3 +195,6 @@ print(confusion_matrix(y_test, predicted_values))
 
 print('Overall f1-score')
 print(f1_score(y_test, predicted_values, average="macro"))
+
+print('Coefficients')
+print(pd.DataFrame(lr.coef_, columns=X.columns).to_string())
